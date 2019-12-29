97119689b342c613826b4795b28a0824fd55bcaf
楠木i <33346285+Zhb1508444061@users.noreply.github.com>
2019-12-29 22:43:48 +0800

    Version 1.2.0 Create Map.java

---
+++ src/org/serverct/ersha/bisai/luckybag/Map.java
@@ -0,0 +1,13 @@
+package org.serverct.ersha.bisai.luckybag;
+
+import java.util.HashMap;
+
+/**
+ * @author ersha
+ * @date 2019/12/29
+ */
+public class Map {
+
+    /**福袋冷却**/
+    public static HashMap<String, String> BagCooling = new HashMap<>();
+}
