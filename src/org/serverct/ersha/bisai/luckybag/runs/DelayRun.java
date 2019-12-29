3c21561bb5e5bb42059284956d0c2d585c6eb9fe
楠木i <33346285+Zhb1508444061@users.noreply.github.com>
2019-12-29 22:12:43 +0800

    Version 1.2.0 Create DelayRun.java

---
+++ src/org/serverct/ersha/bisai/luckybag/runs/DelayRun.java
@@ -0,0 +1,29 @@
+package org.serverct.ersha.bisai.luckybag.runs;
+
+import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.serverct.ersha.bisai.luckybag.Main;
+import org.serverct.ersha.bisai.luckybag.util.Reward;
+
+/**
+ * @author ersha
+ * @date 2019/12/29
+ */
+public class DelayRun extends BukkitRunnable {
+
+    private Player player;
+    private String command;
+
+    public void delayRun(Player player, String command, int time){
+        this.player = player;
+        this.command = command;
+        this.runTaskLater(Main.getInstance(), time*20);
+    }
+
+    @Override
+    public void run() {
+        if (player.isOnline()){
+            new Reward(player).runCommand(command);
+        }
+    }
+}
