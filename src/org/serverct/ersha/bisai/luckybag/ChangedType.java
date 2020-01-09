83c3f71e63c0ef935022c25c458451e3e459c3f8
楠木i <33346285+iNanmu@users.noreply.github.com>
2020-01-09 17:00:33 +0800

    Create ChangedType.java (1.3.0)

---
+++ src/org/serverct/ersha/bisai/luckybag/ChangedType.java
@@ -0,0 +1,14 @@
+package org.serverct.ersha.bisai.luckybag.enumerate;
+
+/**
+ * @author ersha
+ * @date 2020/1/3
+ */
+public enum ChangedType {
+    //增加
+    ADD,
+    //扣除
+    TAKE,
+    //设置
+    SET
+}
