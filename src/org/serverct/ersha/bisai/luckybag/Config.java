74e7a2da5e4e7aa6bdd746332754c20c47f58732
楠木i <33346285+iNanmu@users.noreply.github.com>
2020-01-09 16:47:24 +0800

    update 1.3.0

--- src/org/serverct/ersha/bisai/luckybag/Config.java
+++ src/org/serverct/ersha/bisai/luckybag/Config.java
@@ -1,12 +1,20 @@
 package org.serverct.ersha.bisai.luckybag;
 
+import org.bukkit.configuration.file.FileConfiguration;
+
 /**
  * @author ersha
  * @date 2019/12/1
  */
 public class Config {
 
+    private static FileConfiguration config = Main.getInstance().getConfig();
+
+    public static Object getSetting(String path){
+        return config.get(path);
+    }
+
     public static String getMessage(String yaml){
-        return Main.getInstance().getConfig().getString("Message."+yaml).replace("&", "§");
+        return config.getString("Message."+yaml).replace("&", "§");
     }
 }
