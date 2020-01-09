98451ccc3b832f6ddcf36834b309566984730fc2
楠木i <33346285+iNanmu@users.noreply.github.com>
2020-01-09 16:53:16 +0800

    Create AttributePlus.java (1.3.0)

---
+++ src/org/serverct/ersha/bisai/luckybag/extension/AttributePlus.java
@@ -0,0 +1,52 @@
+package org.serverct.ersha.bisai.luckybag.extension;
+
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
+import org.serverct.ersha.bisai.luckybag.Main;
+import org.serverct.ersha.bisai.luckybag.Map;
+import org.serverct.ersha.jd.AttributeAPI;
+
+import java.io.File;
+import java.util.List;
+
+/**
+ * @author ersha
+ * @date 2020/1/9
+ */
+public class AttributePlus {
+
+    private static File file = new File(Main.getInstance().getDataFolder() + "/attribute.yml");
+    public static YamlConfiguration yaml;
+
+    public AttributePlus() {
+        if (!file.exists()) {
+            Main.getInstance().saveResource("attribute.yml", true);
+        }
+        yaml = YamlConfiguration.loadConfiguration(file);
+    }
+
+    /**
+     * 加载福气属性
+     * @param name 玩家名
+     */
+    public static void loadAttribute(String name) {
+        if (yaml.getBoolean("Setting.enable")) {
+            int dataValue = Main.getDatabase().getPlayerValue(name);
+            List<String> list = yaml.getStringList("Setting.list");
+            for (String str : list) {
+                String[] args = str.split("#");
+                int value = Integer.parseInt(args[0]);
+                List<String> attribute = yaml.getStringList("AttributeGroup." + args[1]);
+                if (dataValue >= value) {
+                    if (Bukkit.getPlayer(name).isOnline()) {
+                        Player player = Bukkit.getPlayer(name);
+                        //调用属性API
+                        AttributeAPI.addAttribute(player, "LuckyBag", attribute, false);
+                        Map.AttributeGroup.put(player, args[1]);
+                    }
+                }
+            }
+        }
+    }
+}
